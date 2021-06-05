679dd0a82a556de610de24e5c7bbf0ce82e92809
Shivam Kapoor <nousernameavailableforu@gmail.com>
2021-06-06 02:54:38 +0530

    Initial commit

---
+++ src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Shivam Kapoor",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Shivam Kapoor and I'm a Software Engineer!",
+  githubUrl: "https://github.com/silverbullet1",
+  linkedinUrl: "https://www.linkedin.com/in/shivamk4r/",
+  cvLink: "https://drive.google.com/file/d/1SATJRRvXtQMR5nrGB7e32XknwrB3Vl6F/",
+};
