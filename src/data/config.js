714b1eae28adf8eb6feb5fe1507621f6075c9533
Shivam Kapoor <nousernameavailableforu@gmail.com>
2021-06-13 18:12:49 +0530

    Update resume

--- src/data/config.js
+++ src/data/config.js
@@ -6,5 +6,5 @@ module.exports = {
   greetingDescription: "I'm Shivam Kapoor and I'm a Software Engineer!",
   githubUrl: "https://github.com/silverbullet1",
   linkedinUrl: "https://www.linkedin.com/in/shivamk4r/",
-  cvLink: "https://drive.google.com/file/d/1ysfKjWkcJ9kU4_AxvPC3TKVPUShC0ahp/view?usp=sharing",
+  cvLink: "https://drive.google.com/file/d/1dKm2WmZgvnFhNNWKsfJtzujH5r1oHL-e/view?usp=sharing",
 };
